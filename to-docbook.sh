853ecbd4416f775c025beb5691197caf0c75c54e
Michael <michael@snoyman.com>
2012-02-24 10:04:17 +0200

    to-docbook.sh: relative paths

--- to-docbook.sh
+++ to-docbook.sh
@@ -7,6 +7,6 @@ cp yesod.xml ../oreilly/book.xml
 cd book/yesod-web-framework-book
 for f in $(find . -name \*.png)
 do
-    mkdir -p ~/haskell/oreilly/images/$(dirname $f)
-    cp $f ~/haskell/oreilly/images/$f
+    mkdir -p ../oreilly/images/$(dirname $f)
+    cp $f ../oreilly/images/$f
 done
